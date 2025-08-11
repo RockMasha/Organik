3a4d7a992e5668f71971057e0cc2159097ec9cbf
IVoynelovych <deafreak89@gmail.com>
2025-08-11 09:55:30 +0300

    fix: small fix hero
    Small fixes in hero and styles

--- src/widgets/HeroSection/components/HeroSection.styled.tsx
+++ src/widgets/HeroSection/components/HeroSection.styled.tsx
@@ -2,10 +2,9 @@ import styled from '@emotion/styled'
 import responsiveSizing from '@/styles/shared/resonsiceSizing'
 import Title from '@/components/ui/Title'
 import { Button } from '@/components/ui/button'
+import Container from '@/components/ui/Container'
 export const Section = styled.section`
   margin-top: ${responsiveSizing(30, 47, 320, 768)};
-  padding: ${responsiveSizing(79, 110, 320, 768)}
-    ${responsiveSizing(20, 30, 320, 768)} ${responsiveSizing(90, 120, 320, 768)};
   text-align: left;
   background-size: cover;
   background-image: image-set(
@@ -21,12 +20,8 @@ export const Section = styled.section`
     url('/images/choose-the-best/choose-the-best-320-2x.jpg') 2x
   );
   background-repeat: no-repeat;
-
   @media screen and (min-width: 768px) {
     margin-top: ${responsiveSizing(47, 40, 768, 1440)};
-    padding: ${responsiveSizing(100, 220, 768, 1440)} 30px
-      ${responsiveSizing(120, 250, 768, 1440)};
-
     background-image: image-set(
       url('/images/choose-the-best/choose-the-best-768-1x.avif')
         type('image/avif') 1x,
@@ -43,7 +38,6 @@ export const Section = styled.section`
 
   @media screen and (min-width: 1440px) {
     margin-top: ${responsiveSizing(40, 58, 1440, 1920)};
-    padding: 220px ${responsiveSizing(30, 260, 1440, 1920)} 250px;
     text-align: left;
     background-image: image-set(
       url('/images/choose-the-best/choose-the-best-1440-1x.avif')
@@ -59,7 +53,6 @@ export const Section = styled.section`
     );
     @media screen and (min-width: 1920px) {
       margin-top: 58px;
-      padding: 220px 260px 250px;
       background-image: image-set(
         url('/images/choose-the-best/choose-the-best-1920-1x.avif')
           type('image/avif') 1x,
@@ -75,15 +68,32 @@ export const Section = styled.section`
     }
   }
 `
+export const StyledContainer = styled(Container)`
+  padding: ${responsiveSizing(79, 110, 320, 768)}
+    ${responsiveSizing(20, 30, 320, 768)} ${responsiveSizing(90, 120, 320, 768)};
+  @media screen and (min-width: 768px) {
+    padding: ${responsiveSizing(100, 220, 768, 1440)} 30px
+      ${responsiveSizing(120, 250, 768, 1440)};
+  }
+  @media screen and (min-width: 1440px) {
+    padding: 220px ${responsiveSizing(30, 260, 1440, 1920)} 250px;
+  }
+  @media screen and (min-width: 1920px) {
+    padding: 220px 260px 250px;
+  }
+`
 export const StyledTitle = styled(Title)`
   margin-top: ${responsiveSizing(15, 30, 320, 768)};
-  width: ${responsiveSizing(260, 380, 320, 768)};
+  line-height: 43.2px;
+  height: calc(3 * 43.2px);
   @media screen and (min-width: 768px) {
-    width: ${responsiveSizing(380, 500, 768, 1440)};
+    height: calc(3 * 66px);
+    line-height: 66px;
     margin-top: 30px;
   }
   @media screen and (min-width: 1440px) {
-    width: 500px;
+    height: calc(3 * 84px);
+    line-height: 84px;
   }
 `
 export const StyledButton = styled(Button)`
--- src/widgets/HeroSection/components/HeroSection.tsx
+++ src/widgets/HeroSection/components/HeroSection.tsx
@@ -1,16 +1,23 @@
-import { Section, StyledTitle, StyledButton } from './HeroSection.styled'
+import {
+  Section,
+  StyledTitle,
+  StyledButton,
+  StyledContainer,
+} from './HeroSection.styled'
 import Text from '@/components/ui/Text'
 
 function HeroSection() {
   return (
     <Section id="hero">
-      <Text type="green">Made with nature</Text>
-      <StyledTitle type="h1">
-        Welcome to the world of nature and organic.
-      </StyledTitle>
-      <StyledButton size={'flattened'} variant={'yellow'} arrow={true}>
-        Explore Now
-      </StyledButton>
+      <StyledContainer>
+        <Text type="green">Made with nature</Text>
+        <StyledTitle type="h1">
+          Welcome to the world of nature and organic.
+        </StyledTitle>
+        <StyledButton size={'flattened'} variant={'yellow'} arrow={true}>
+          Explore Now
+        </StyledButton>
+      </StyledContainer>
     </Section>
   )
 }

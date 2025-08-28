8778c1738ea912f86479682f6e182e7344438a81
Maria <maryallbond@gmail.com>
2025-08-28 17:28:19 +0300

    fix: redact loadings

--- src/features/RedactForm/hooks/useRedactForm.ts
+++ src/features/RedactForm/hooks/useRedactForm.ts
@@ -1,7 +1,7 @@
 import { useAppDispatch } from '@/shared/hooks/useAppDispatch'
 import { useNavigate } from 'react-router-dom'
 import { RedactSchema, type Redact } from '../consts/RedactSchema'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { refreshUser } from '@/api/refreshUser'
 import { useForm, type UseFormReturn } from 'react-hook-form'
 import useLoading from '@/shared/hooks/useLoading'
@@ -36,8 +36,8 @@ const resetForm = ({ methods, data }: resetFormProps) => {
 }
 
 export const useRedactForm = () => {
-  const [loadingFields, setLoadingFields] = useState(true)
-  const [loading, startLoading] = useLoading()
+  const [postOrderLoading, startPostOrderLoading] = useLoading()
+  const [fetchUserLoading, startFetchUserLoading] = useLoading()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
   const methods = useForm<Redact>({
@@ -47,18 +47,19 @@ export const useRedactForm = () => {
   })
 
   useEffect(() => {
-    startLoading(async () => {
+    startFetchUserLoading(async () => {
       const user = await dispatch(refreshUser()).unwrap()
       resetForm({ methods, data: user })
-      setLoadingFields(false)
     })
   }, [methods])
 
   const onSubmit = async (data: Redact) => {
-    const response = await dispatch(redactUser(data)).unwrap()
-    resetForm({ methods, data: response })
-    navigate(getRoute('home'))
+    startPostOrderLoading(async () => {
+      const response = await dispatch(redactUser(data)).unwrap()
+      resetForm({ methods, data: response })
+      navigate(getRoute('home'))
+    })
   }
 
-  return { methods, onSubmit, loading, loadingFields }
+  return { methods, onSubmit, postOrderLoading, fetchUserLoading }
 }

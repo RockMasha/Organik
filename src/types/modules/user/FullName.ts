c8a48508403b9192901b1f8bd4f7c4f65fb28c8b
Maria <maryallbond@gmail.com>
2025-08-28 17:23:24 +0300

    fix: change text area focus ring color

--- src/types/modules/user/FullName.ts
+++ src/types/modules/user/FullName.ts
@@ -1,7 +1,7 @@
 import { string } from 'zod'
 
 export const FullNameSchema = string('')
-  .min(4, 'min symbols 2')
+  .min(4, 'min symbols 4')
   .max(50, 'max symbols 50')
   .refine((val) => val.trim().split(/\s+/).length === 2, {
     message: 'Name must contain two word: name and surname',
